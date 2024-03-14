3315715e1d46a2a365a7748644fd5a944b096a54
HsinJe Henry Ho <jeho1993@hotmail.com>
2024-03-14 12:57:31 +0800

    Add unnamed pattern

--- src/java21/Unnamed/Main.java
+++ src/java21/Unnamed/Main.java
@@ -1,19 +1,53 @@
 
 //java --source 21 --enable-preview .\Main.java
 
+import java.util.List;
 
-static void main() {
+public record Car(
+    String name, String color) {
 
-    if(staticVariable) {
+  }
+
+  static void main() {
+
+    if (staticVariable) {
       System.out.println("static variable");
     }
-    
+
     staticMethod();
-}
 
+    List<Integer> test = List.of(1, 2, 3);
+    for (var _ : test) {
+      System.out.println("count");
+    }
 
-static boolean staticVariable = true;
-  
-  static void staticMethod(){
+    try {
+      throw new Exception("");
+    } catch (Exception _) {
+      System.out.println("abc");
+    }
+
+    var car = new Car("abc", "blue");
+    System.out.println(getObjectsColorWithUnnamedPattern(car));
+    System.out.println(getObjectsColorWithSwitchAndUnnamedPattern(car));
+  }
+
+  static boolean staticVariable = true;
+
+  static void staticMethod() {
     System.out.println("static method");
   }
+
+  static String getObjectsColorWithUnnamedPattern(Object object) {
+    if (object instanceof Car(_, String color)) {
+        return color;
+    }
+    return "No color!";
+}
+
+static String getObjectsColorWithSwitchAndUnnamedPattern(Object object) {
+  return switch (object) {
+      case Car(_, String color) -> color;
+      default -> "No color!";
+  };
+}
